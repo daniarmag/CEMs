4cb413334b77db3963500f184b5df13f3852d91e
Matan <cz.matan2@gmail.com>
2023-05-10 12:28:13 +0300

    test

---
+++ Commit/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("test");
+		System.out.println("check");
+		System.out.println("check 2");
+		System.out.println("matan");
+	}
+	
+}
