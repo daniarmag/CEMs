659d89bc93571969cd45e98405db3d3ff4bec74f
daniarmag <daniarmag@gmail.com>
2023-05-10 20:00:44 +0300

    SQL stuff

--- CEMs/src/server/MySQLConnection.java
+++ CEMs/src/server/MySQLConnection.java
@@ -1,5 +1,34 @@
 package server;
 
-public class MySQLConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySQLConnection 
+{
+	static Connection conn;
+	
+	public static void connectToDB() 
+	{
+		try 
+		{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver definition succeed");
+        } catch (Exception ex) {
+        	/* handle the error*/
+        	 System.out.println("Driver definition failed");
+        	 }
+        
+        try 
+        {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","Daniel123456!");
+            System.out.println("SQL connection succeed");
+
+     	} catch (SQLException ex) 
+     	    {/* handle any errors*/
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            }
+   	}
 }
