a60910c00c6b196e6392ea7757ebc8896f27bc55
daniarmag <daniarmag@gmail.com>
2023-05-11 01:29:35 +0300

    ServerGUI works

--- CEMs/src/gui/ServerScreenController.java
+++ CEMs/src/gui/ServerScreenController.java
@@ -1,33 +1,59 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class ServerScreenController 
+public class ServerScreenController implements Initializable 
 {
-    @FXML
-    private Button connectBtn;
+	@FXML
+	private Button connectBtn;
 
-    @FXML
-    private Button disconnectBtn;
+	@FXML
+	private Button disconnectBtn;
 
-    @FXML
-    private Button exitBtn;
+	@FXML
+	private Button exitBtn;
 
-    @FXML
-    private TextField textAreaPassword;
+	@FXML
+	private TextField textAreaPassword;
 
-    @FXML
-    private TextField txtAreaDbName;
+	@FXML
+	private TextField txtAreaDbName;
 
-    @FXML
-    private TextField txtAreaIP;
+	@FXML
+	private TextField txtAreaIP;
 
-    @FXML
-    private TextField txtAreaPort;
+	@FXML
+	private TextField txtAreaPort;
 
-    @FXML
-    private TextField txtAreaUsername;
+	@FXML
+	private TextField txtAreaUsername;
+
+	public void start(Stage primaryStage) throws Exception
+	{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/gui/ServerScreen.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Server");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
 }
-
--- CEMs/src/server/ServerUI.java
+++ CEMs/src/server/ServerUI.java
@@ -1,5 +1,49 @@
 package server;
 
-public class ServerUI {
+import gui.ServerScreenController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import server.EchoServer;
+
+public class ServerUI extends Application {
+	final public static int DEFAULT_PORT = 5555;
+
+	public static void main(String args[]) throws Exception
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		ServerScreenController serverScreen = new ServerScreenController();
+		serverScreen.start(primaryStage);
+	}
+
+	public static void runServer(String p) 
+	{
+		int port = 0; // Port to listen on
+		try
+		{
+			port = Integer.parseInt(p); // Set port to 5555
+
+		} catch (Throwable t) 
+		{
+			System.out.println("ERROR - Could not connect!");
+		}
+
+		EchoServer sv = new EchoServer(port);
+
+		try 
+		{
+			sv.listen(); // Start listening for connections
+		} catch (Exception ex)
+		{
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+	}
 
 }
