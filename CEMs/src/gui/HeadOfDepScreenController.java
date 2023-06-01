b19372a2d0b2769320d10c72743c365ac055e7c1
Matan <cz.matan2@gmail.com>
2023-06-01 20:03:25 +0300

    Head of fepartment statring

---
+++ CEMs/src/gui/HeadOfDepScreenController.java
@@ -0,0 +1,42 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class HeadOfDepScreenController {
+
+    @FXML
+    private Text Alert;
+
+    @FXML
+    private Button checkResBtn;
+
+    @FXML
+    private Button createExamBtn;
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button manageQstBtn;
+
+    @FXML
+    private Text welcomeText;
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+}
+
