3ec8331aa29419d6707675cfde6d5db2af43fadc
daniarmag <daniarmag@gmail.com>
2023-06-18 21:09:59 +0300

    test

--- CEMs/src/client/ClientMessageHandler.java
+++ CEMs/src/client/ClientMessageHandler.java
@@ -65,7 +65,6 @@ public class ClientMessageHandler
 		headOfDepartmentScreenController = new HeadOfDepartmentScreenController();
 	}
 	
-	
 	/**
 	 * @param headOfDepartmentScreenController the headOfDepartmentScreenController to set
 	 */
